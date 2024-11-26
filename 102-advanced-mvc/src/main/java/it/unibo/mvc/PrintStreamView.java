934924937c963a83e14f9e7b8060eda5ce603434
Eugenio Guidi <eugenio.guidi@studio.unibo.it>
2024-11-26 18:04:10 +0100

    In exercise 102 in Class PrintStreamView implemented method displayError

--- 102-advanced-mvc/src/main/java/it/unibo/mvc/PrintStreamView.java
+++ 102-advanced-mvc/src/main/java/it/unibo/mvc/PrintStreamView.java
@@ -60,7 +60,7 @@ public final class PrintStreamView implements DrawNumberView {
 
     @Override
     public void displayError(String message) {
-        
+        System.out.println("error!\n" + message);
     }
 
 }
